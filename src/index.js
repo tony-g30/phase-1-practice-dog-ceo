e81f1cac98a44706317b0edfba8beffb3e58d87f
tony-g30 <tonyaudo@gmail.com>
2024-07-24 01:18:02 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+    const imageContainer = document.getElementById('dog-image-container');
+    data.message.forEach(imageUrl => {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    imageContainer.appendChild(img);
+    });
+    });
+    });
